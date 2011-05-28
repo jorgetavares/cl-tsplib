90f9c22c929a8094b2213665a3957eb35024bacb
Jorge Tavares <jorge.tavares@ieee.org>
2011-05-28 22:41:38 +0100

    Add parsers for lower-diag-row, full-matrix, upper-row and display data.

--- tsplib.lisp
+++ tsplib.lisp
@@ -8,13 +8,36 @@
 ;;; constants
 ;;;
 
-(defparameter *eil51*   "/Users/jast/workspace/datasets/tsplib/tsp/eil51.tsp")
-(defparameter *burma14* "/Users/jast/workspace/datasets/tsplib/tsp/burma14.tsp")
-(defparameter *kroA100* "/Users/jast/workspace/datasets/tsplib/tsp/kroA100.tsp")
-(defparameter *d198*    "/Users/jast/workspace/datasets/tsplib/tsp/d198.tsp")
-(defparameter *lin318*  "/Users/jast/workspace/datasets/tsplib/tsp/lin318.tsp")
-(defparameter *pcb442*  "/Users/jast/workspace/datasets/tsplib/tsp/pcb442.tsp")
-(defparameter *att532*  "/Users/jast/workspace/datasets/tsplib/tsp/att532.tsp")
+;; euc_2d
+(defparameter *eil51*    "/Users/jast/workspace/ecos/datasets/tsplib/tsp/eil51.tsp")
+(defparameter *burma14*  "/Users/jast/workspace/ecos/datasets/tsplib/tsp/burma14.tsp")
+(defparameter *kroA100*  "/Users/jast/workspace/ecos/datasets/tsplib/tsp/kroA100.tsp")
+(defparameter *d198*     "/Users/jast/workspace/ecos/datasets/tsplib/tsp/d198.tsp")
+(defparameter *lin318*   "/Users/jast/workspace/ecos/datasets/tsplib/tsp/lin318.tsp")
+(defparameter *pcb442*   "/Users/jast/workspace/ecos/datasets/tsplib/tsp/pcb442.tsp")
+(defparameter *berlin52* "/Users/jast/workspace/ecos/datasets/tsplib/tsp/berlin52.tsp")
+
+;; att
+(defparameter *att532*    "/Users/jast/workspace/ecos/datasets/tsplib/tsp/att532.tsp")
+
+;; geo
+(defparameter *ulysses16* "/Users/jast/workspace/ecos/datasets/tsplib/tsp/ulysses16.tsp")
+(defparameter *ulysses22* "/Users/jast/workspace/ecos/datasets/tsplib/tsp/ulysses22.tsp")
+
+;; explicit
+; lower-diag-row
+(defparameter *fri26*     "/Users/jast/workspace/ecos/datasets/tsplib/tsp/fri26.tsp")
+(defparameter *gr17*      "/Users/jast/workspace/ecos/datasets/tsplib/tsp/gr17.tsp")
+(defparameter *gr21*      "/Users/jast/workspace/ecos/datasets/tsplib/tsp/gr21.tsp")
+(defparameter *gr24*      "/Users/jast/workspace/ecos/datasets/tsplib/tsp/gr24.tsp")
+(defparameter *gr48*      "/Users/jast/workspace/ecos/datasets/tsplib/tsp/gr48.tsp")
+(defparameter *hk48*      "/Users/jast/workspace/ecos/datasets/tsplib/tsp/hk48.tsp")
+(defparameter *dantzig42* "/Users/jast/workspace/ecos/datasets/tsplib/tsp/dantzig42.tsp")
+; full matrix
+(defparameter *swiss42* "/Users/jast/workspace/ecos/datasets/tsplib/tsp/swiss42.tsp")
+(defparameter *bays29*  "/Users/jast/workspace/ecos/datasets/tsplib/tsp/bays29.tsp")
+; upper row
+(defparameter *bayg29* "/Users/jast/workspace/ecos/datasets/tsplib/tsp/bayg29.tsp")
 
 
 
@@ -42,29 +65,39 @@
   "Prints the TSPlib problem instance."
   (with-open-file (stream file :direction :input)
     (loop for line = (read-line stream nil nil)
-       while line do (format t "~a~%" line))))
+	  while line do (format t "~a~%" line))))
 
 (defun parse-problem-instance (file)
   "Parses a TSPlib problem instance."
   (let ((instance (make-problem-instance)))
     (with-open-file (stream file :direction :input)
-      (loop 
-	 for line = (trim (read-line stream nil nil))
-	 until (node-coordinates-p line)
-	 do (cond ((name-p         line) (parse-name         instance line))
-		  ((comment-p      line) (parse-comment      instance line))
-		  ((type-p         line) (parse-type         instance line))
-		  ((dimension-p    line) (parse-dimension    instance line))
-		  ((edge-type-p    line) (parse-edge-type    instance line))
-		  ((edge-format-p  line) (parse-edge-format  instance line))
-		  ((display-data-p line) (parse-display-data instance line))))
-      (parse-node-coordinates instance stream)
-      (setf (problem-instance-distance-matrix instance)
-	    (compute-distance-matrix
-	     (problem-instance-nodes-coordinates instance)
-	     (problem-instance-dimension instance)
-	     (get-distance-function 
-	      (problem-instance-edge-weight-type instance)))))
+      (loop for line = (trim (read-line stream nil nil))
+	    until (or (node-coordinates-p line)
+		      (edge-weights-p line))
+	    do (cond ((name-p         line) (parse-name         instance line))
+		     ((comment-p      line) (parse-comment      instance line))
+		     ((type-p         line) (parse-type         instance line))
+		     ((dimension-p    line) (parse-dimension    instance line))
+		     ((edge-type-p    line) (parse-edge-type    instance line))
+		     ((edge-format-p  line) (parse-edge-format  instance line))
+		     ((display-data-p line) (parse-display-data instance line)))
+	    finally (cond ((node-coordinates-p line)
+			   (progn
+			     (parse-node-coordinates instance stream)
+			     (setf (problem-instance-distance-matrix instance)
+				   (compute-distance-matrix
+				    (problem-instance-nodes-coordinates instance)
+				    (problem-instance-dimension instance)
+				    (get-distance-function 
+				     (problem-instance-edge-weight-type instance))))))
+			  ((edge-weights-p line)
+			   (progn
+			     (setf (problem-instance-distance-matrix instance)
+				   (parse-edge-weights instance stream))
+			     (when (problem-instance-display-data-type instance)
+			        (loop for line = (read-line stream nil nil)
+				      until (string= "DISPLAY_DATA_SECTION" line)
+				      finally (parse-node-coordinates instance stream))))))))
     instance))
 
 (defun trim (string)
@@ -129,7 +162,8 @@
 (defparameter *edge-type* "EDGE_WEIGHT_TYPE")
 (defparameter *edge-weight-types* (list (cons "EUC_2D" :euc-2d) 
 					(cons "GEO" :geo)
-					(cons "ATT" :att)))
+					(cons "ATT" :att)
+					(cons "EXPLICIT" :explicit)))
 
 (defun edge-type-p (line)
   (string= *edge-type* line :end2 16))
@@ -142,7 +176,10 @@
 
 
 (defparameter *edge-format* "EDGE_WEIGHT_FORMAT")
-(defparameter *edge-weight-formats* (list (cons "FUNCTION" :function)))
+(defparameter *edge-weight-formats* (list (cons "FUNCTION" :function)
+					  (cons "LOWER_DIAG_ROW" :lower-diag-row)
+					  (cons "FULL_MATRIX" :full-matrix)
+					  (cons "UPPER_ROW" :upper-row)))
 
 (defun edge-format-p (line)
   (string= *edge-format* line :end2 18))
@@ -154,7 +191,8 @@
 		    *edge-weight-formats* :test #'string=))))
 
 (defparameter *display-data* "DISPLAY_DATA_TYPE")
-(defparameter *display-data-types* (cons (cons "COORD_DISPLAY" :coord) '()))
+(defparameter *display-data-types* (list (cons "COORD_DISPLAY" :coord)
+					 (cons "TWOD_DISPLAY"  :twod)))
 
 (defun display-data-p (line)
   (string= *display-data* line :end2 17))
@@ -165,7 +203,52 @@
 	(cdr (assoc (filter-start (subseq line (mismatch *display-data* line)))
 		    *display-data-types* :test #'string=))))
 
+(defparameter *edge-weights* "EDGE_WEIGHT_SECTION")
+
+(defun edge-weights-p (line)
+  "Check if is about to enter the edge weights section."
+  (when (>= (length line) 19) 
+    (string= *edge-weights* (subseq line 0 19))))
+
+(defun parse-edge-weights (instance stream)
+  ""
+  (let* ((dimension (problem-instance-dimension instance))
+	 (distances (make-array `(,(+ 1 dimension) ,(+ 1 dimension))
+			       :initial-element 0)))
+    (case (problem-instance-edge-weight-format instance)
+      (:lower-diag-row (parse-lower-diag  stream dimension distances))
+      (:full-matrix    (parse-full-matrix stream dimension distances))
+      (:upper-row      (parse-upper-row   stream dimension distances)))))
+
+(defun parse-lower-diag (stream dimension distances)
+  (loop for i from 1 to dimension
+	do (loop for j from 1 to i
+		 do (let ((weight (read stream nil nil)))
+		      (setf (aref distances i j) weight)
+		      (setf (aref distances j i) weight)))
+	finally (return distances)))
+
+(defun parse-full-matrix (stream dimension distances)
+  (loop for i from 1 to dimension
+	do (loop for j from 1 to dimension
+		 do (setf (aref distances j i) (read stream nil nil)))
+	finally (return distances)))
+
+(defun parse-upper-row (stream dimension distances)
+  (loop for i from 1 below dimension
+	for k downfrom dimension to 1
+	do (loop for j from 1 below k
+		 for l downfrom dimension to 2 
+		 do (let ((weight (read stream nil nil)))
+		      (setf (aref distances i j) weight)
+		      (setf (aref distances k l) weight)))
+	finally (return distances)))
+
 (defparameter *nodes-coordinates* "NODE_COORD_SECTION")
+
+(defun node-coordinates-p (line)
+  "Check if is about to enter the nodes coordinates section."
+  (string= *nodes-coordinates* line))
 
 (defun parse-coordinate (line)
   "Returns the node, x and y coordinates."
@@ -176,23 +259,18 @@
 	   (setq string (subseq string start))
 	   (setq position start) value))))
 
-(defun node-coordinates-p (line)
-  "Chaeck if is about to enter the nodes coordinates section."
-  (string= *nodes-coordinates* line))
-
 (defun parse-node-coordinates (instance stream)
   "Checks if is the coordinates section and reads them."
   (let ((coordinates (make-array 
 		      `(,(+ 1 (problem-instance-dimension instance)) 2)
 		      :initial-element 0)))
-    (loop 
-       for line = (read-line stream nil nil)
-       until (string= line "EOF")
-       while line do (let* ((values (parse-coordinate line))
-			    (node (first values)) 
-			    (x (second values)) (y (third values)))
-		       (setf (aref coordinates node 0) x
-			     (aref coordinates node 1) y)))
+    (loop for line = (read-line stream nil nil)
+	  until (string= line "EOF")
+	  while line do (let* ((values (parse-coordinate line))
+			       (node (first values)) 
+			       (x (second values)) (y (third values)))
+			  (setf (aref coordinates node 0) x
+				(aref coordinates node 1) y)))
     (setf (problem-instance-nodes-coordinates instance) coordinates)))
 
 
@@ -211,15 +289,15 @@
   (let ((distances (make-array `(,(+ 1 dimension) ,(+ 1 dimension)) 
 			       :initial-element 0)))
     (loop for i from 1 to dimension 
-       do (let ((X (node-coordinates i coordinates))) 
-	    (loop for j from i to dimension 
-	       do (let* ((Y (node-coordinates j coordinates))
-			 (distance (funcall fn-distance 
-					    (first X) (first Y)
-					    (second X) (second Y)))) 
-		    (setf (aref distances i j) distance
-			  (aref distances j i) distance))))
-       finally (return distances))))
+	  do (let ((X (node-coordinates i coordinates))) 
+	       (loop for j from i to dimension 
+		     do (let* ((Y (node-coordinates j coordinates))
+			       (distance (funcall fn-distance 
+						  (first X) (first Y)
+						  (second X) (second Y)))) 
+			  (setf (aref distances i j) distance
+				(aref distances j i) distance))))
+	  finally (return distances))))
 
 (defun node-coordinates (node coordinates)
   "Return the x and y of a node."
@@ -286,25 +364,19 @@
   "Returns the list of the nearest enighbors for a set of cities."
   (let ((nearest-list (make-array `(,(1+ n) ,(1+ n-neighbors)) :initial-element 0))
 	(all-neighbors (compute-nearest-neighbors distances n)))
-    (loop 
-       for i from 1 to n
-       for neighbors in all-neighbors
-       do (loop for j from 1 to n-neighbors
-	     do (setf (aref nearest-list i j) 
-		      (nth j neighbors)))
-       finally (return nearest-list))))
+    (loop for i from 1 to n
+	  for neighbors in all-neighbors
+	  do (loop for j from 1 to n-neighbors
+		   do (setf (aref nearest-list i j) 
+			    (nth j neighbors)))
+	  finally (return nearest-list))))
     
 (defun compute-nearest-neighbors (distances n)
   "Computes all the nearest neighbors given a distance matrix."
   (loop with indexes = (loop for i from 1 to n collect i) 
-     for i from 1 to n
-     collect (loop for j from 0 to n
-		collect (aref distances i j) into distances-list
-		finally (return (remove i (sort (copy-list indexes) #'< 
-						:key #'(lambda (x) 
-							 (nth x distances-list))))))))
-
-
-
-
-
+	for i from 1 to n
+	collect (loop for j from 0 to n
+		      collect (aref distances i j) into distances-list
+		      finally (return (remove i (sort (copy-list indexes) #'< 
+						      :key #'(lambda (x) 
+							       (nth x distances-list))))))))
